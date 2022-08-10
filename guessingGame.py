b0b0be9d24ce90fdfda7e418850e5db19a52013a
tina297 <74814611+tina297@users.noreply.github.com>
2022-08-10 09:06:49 +0100

    Add files via upload

--- guessingGame.py
+++ guessingGame.py
@@ -3,19 +3,19 @@ number = random.randint(1,9)
 #number = 5
 chance = 0
 guess = int(input("Guess a number between 1 and 9: "))
-chance = chance + 1
 
-while chance < 5:
-  chance = chance + 1
 
-  if number == guess:
-    print("YOU GOT IT! WELL DONE!!!")
-    break
-  elif number > guess:
-    guess = int(input("Guess is too low! Guess again... "))
-  elif number < guess:
-    guess = int(input("Guess is too high! Guess again... "))
+while chance < 5:    
+    if guess == number:
+      print("YOU GOT IT! WELL DONE!!!")
+      break
+    elif number > guess:
+        guess = int(input("Guess is too low! Guess again... "))
+    elif number < guess:
+        guess = int(input("Guess is too high! Guess again... "))
 
-  if not chance < 5:
-    print("YOU LOSE!! The number was", number)
+    chance = chance + 1
+
+    if not chance < 5:
+        print("YOU LOSE!! The number was", number)
 
